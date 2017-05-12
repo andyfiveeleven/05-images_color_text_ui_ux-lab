760060ff50c08d1ef9434e71a6d781b538bcfa51
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-12 15:02:33 -0700

    cheekiness, also added arguments

---
+++ tests.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function testSum() { //eslint-disable-line
+  if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
+    console.log('%c TEST FOR sum() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sum() FAILS', 'color: red');
+  }
+}
+
+function testMultiply() { //eslint-disable-line
+  if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+    console.log('%c TEST FOR multiply() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiply() FAILS', 'color: red');
+  }
+}
